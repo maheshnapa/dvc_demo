4e9941e087b4cb369ef2869baee2394fed579f49
Mahesh Napa <Mahesh.Napa@ey.com>
2021-10-14 09:23:08 +0530

    stage 01

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -18,6 +18,7 @@ def get_data(config_path):
     raw_local_file = config['artifacts']['raw_local_file']
 
     raw_local_dir_path = os.path.join(artifacts_dir, raw_local_dir)
+
     create_directory(dirs= [raw_local_dir_path])
     raw_local_file_path = os.path.join(raw_local_dir_path, raw_local_file)
 
@@ -27,4 +28,5 @@ if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default = "config/config.yaml")
     parsed_args = args.parse_args()
+
     get_data(config_path = parsed_args.config)
